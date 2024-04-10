b523da2b40b78e27cb67d615da0560415a87b684
sarahblincko <129764285+sarahblincko@users.noreply.github.com>
2024-04-10 14:58:43 +0100

    Update wk_1_answers_r.R

--- src/coding-challenges/wk_1_answers_r.R
+++ src/coding-challenges/wk_1_answers_r.R
@@ -39,7 +39,6 @@ print(format_date("31/12/2019"))
 print(format_date("15/01/2019"))
 
 # Hard Challenge
-# Hard:
 # An identity matrix is defined as a square matrix with 1s running from the top left of the square to the bottom right. The rest are 0s. The identity matrix has applications ranging from machine learning to the general theory of relativity.
 # Create a function that takes an integer n and returns the identity matrix of n x n dimensions. For this challenge, if the integer is negative, return the mirror image of the identity matrix of n x n dimensions.. It does not matter if the mirror image is left-right or top-bottom.
 # Examples:
