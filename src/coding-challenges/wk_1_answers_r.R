0f8509800e9c56cfc0225322aaeedfad8c2f9a33
sarahblincko <129764285+sarahblincko@users.noreply.github.com>
2024-04-10 14:58:29 +0100

    Update wk_1_answers_r.R

--- src/coding-challenges/wk_1_answers_r.R
+++ src/coding-challenges/wk_1_answers_r.R
@@ -1,3 +1,4 @@
+# Easy Challenge
 # Create a function that takes two arguments: the original price and the discount percentage as integers and returns the final price after the discount.
  
 # Examples:
@@ -16,6 +17,18 @@ print(dis(1500, 50))
 print(dis(89, 20))
 print(dis(100, 75))
 
+# Medium Challenge:
+# Create a function that converts a date formatted as DD/MM/YYYY to YYYYDDMM.
+
+# Examples:
+# format_date("11/12/2019") ➞ "20191211"
+# format_date("31/12/2019") ➞ "20193112"
+# format_date("15/01/2019") ➞ "20191501"
+
+# Notes:
+# Return value should be a string.
+
+
 format_date <- function(date){
     d = strsplit(date, "/")[[1]]
     return(paste0(d[3], d[2], d[1]))
@@ -24,6 +37,24 @@ format_date <- function(date){
 print(format_date("11/12/2019"))
 print(format_date("31/12/2019"))
 print(format_date("15/01/2019"))
+
+# Hard Challenge
+# Hard:
+# An identity matrix is defined as a square matrix with 1s running from the top left of the square to the bottom right. The rest are 0s. The identity matrix has applications ranging from machine learning to the general theory of relativity.
+# Create a function that takes an integer n and returns the identity matrix of n x n dimensions. For this challenge, if the integer is negative, return the mirror image of the identity matrix of n x n dimensions.. It does not matter if the mirror image is left-right or top-bottom.
+# Examples:
+# id_mtrx(2) ➞ [
+#   [1, 0],
+#   [0, 1]
+# ]
+# id_mtrx(-2) ➞ [
+#   [0, 1],
+#   [1, 0]
+# ]
+# id_mtrx(0) ➞ []
+
+# Notes:
+# Incompatible types passed as n should return the string "Error".
 
 id_mtrx <- function(n){
     if (!is.integer(n)) return("Error")
