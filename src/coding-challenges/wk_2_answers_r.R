03a9990b9241043e0387e9aec28bdf62e83f2ab4
Edmund Haacke <edmundhaacke@me.com>
2024-04-25 11:49:59 +0100

    add wk 2 hard R challenge

--- src/coding-challenges/wk_2_answers_r.R
+++ src/coding-challenges/wk_2_answers_r.R
@@ -9,19 +9,19 @@
 # Notes
 # If the input tries to divide by 0, return: "Can't divide by 0!"
 
-calculator<-function(value1,operator,value2){
-  value<-case_when(operator=="+"~ value1+value2,
-            operator=="-"~ value1-value2,
-            operator=="*"~ value1*value2,
-            operator=="/" & value2==0 ~ NA,
-            operator=="/" & value2!=0 ~ value1/value2
-        )
-  case_when(is.na(value)~"Can't divide by 0!",
-            !is.na(value)~ as.character(value))
+# calculator<-function(value1,operator,value2){
+#   value<-case_when(operator=="+"~ value1+value2,
+#             operator=="-"~ value1-value2,
+#             operator=="*"~ value1*value2,
+#             operator=="/" & value2==0 ~ NA,
+#             operator=="/" & value2!=0 ~ value1/value2
+#         )
+#   case_when(is.na(value)~"Can't divide by 0!",
+#             !is.na(value)~ as.character(value))
   
-}
+# }
 
-calculator(2,"*",0)
+# calculator(2,"*",0)
 
 
 # Medium
@@ -53,12 +53,57 @@ calculator(2,"*",0)
 # Profit = Total Sales - Total Cost
 
 
-profit<-function(dictionary){
-  cost_price<-dictionary[["cost_price"]]
-  sell_price<-dictionary[["sell_price"]]
-  inventory<-dictionary[["inventory"]]
-  round((sell_price-cost_price)*inventory,0)
+# profit<-function(dictionary){
+#   cost_price<-dictionary[["cost_price"]]
+#   sell_price<-dictionary[["sell_price"]]
+#   inventory<-dictionary[["inventory"]]
+#   round((sell_price-cost_price)*inventory,0)
 
-  }
+#   }
 
-profit(list(cost_price=32.67,sell_price=45.00,inventory=1200))
+# profit(list(cost_price=32.67,sell_price=45.00,inventory=1200))
+
+
+# Hard:
+# Write a function that returns True if a string consists of ascending or ascending AND consecutive numbers.
+ 
+# Examples
+# ascending("232425") ➞ True
+# (Consecutive numbers 23, 24, 25)
+
+# ascending("2324256") ➞ False
+# (No matter how this string is divided, the numbers are not consecutive)
+ 
+# ascending("444445") ➞ True
+# (Consecutive numbers 444 and 445)
+ 
+# Notes
+# A number can consist of any number of digits, so long as the numbers are adjacent to each other, and the string has at least two of them.
+
+ascending <- function(s){
+    # break down string into different length batches
+    for (n in 1:(floor(nchar(s)/2) + 1)){
+        # create list of s split into lengths
+        grps <- unlist(lapply(seq(0, nchar(s), n), function(i){
+            return(substr(s, (i+1), (i+n)))
+        }))
+        # remove any blanks
+        grps <- grps[grps != ""]
+        # check at least 2 elements in 
+        if (length(grps) < 2) next;
+        # iterate through each element, and check whether the next element is the ascended
+        # version of the current element
+        is_asc <- unlist(lapply(seq(1, length(grps)), function(i){
+            # check within bounds
+            if (i + 1 > length(grps)) return(TRUE)
+            return((as.integer(grps[i]) + 1) == (as.integer(grps[i+1])))
+        }))
+        all_asc <- Reduce("&", is_asc)
+        if (all_asc) return(TRUE)
+    }
+    return(FALSE)
+}
+
+print(ascending("232425"))
+print(ascending("2324256"))
+print(ascending("444445"))
